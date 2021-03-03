f61ee468b75db06f0ac9d84ee22540310bf48c26
Dayo Akinkuowo <me@sprypixels.com>
2021-03-03 20:37:17 +0100

    Installed react-native-maps package

---
+++ src/screens/dashboard/order/Detail.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { detailStyles as styles } from './styles';
+
+const OrderDetail = () => {
+  return <View style={styles.container}></View>;
+};
+
+export default OrderDetail;
