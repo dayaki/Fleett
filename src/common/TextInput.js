9f0572bbfa5b09b3dcfa9aba11aaf28c68f86ef4
Dayo Akinkuowo <me@sprypixels.com>
2021-03-13 02:13:17 +0100

    Added more props for error and input

--- src/common/TextInput.js
+++ src/common/TextInput.js
@@ -3,14 +3,25 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import * as Colors from './Colors';
 import { hp, wp } from './utils';
 
-export const Input = ({ placeholder, icon }) => (
+export const Input = ({
+  placeholder,
+  icon,
+  value,
+  onChange,
+  onBlur,
+  error,
+}) => (
   <View style={styles.input}>
     <View style={styles.icon}>{icon}</View>
     <TextInput
+      value={value}
+      onChangeText={onChange}
+      onBlur={onBlur}
       placeholder={placeholder}
       placeholderTextColor={Colors.SilverGray}
       style={styles.textInput}
     />
+    {error && <Text style={styles.error}>{error}</Text>}
   </View>
 );
 
@@ -32,5 +43,11 @@ const styles = StyleSheet.create({
     lineHeight: hp(21),
     color: Colors.SilverChalice,
     paddingLeft: wp(8),
+  },
+  error: {
+    fontFamily: 'TTNormsPro-Regular',
+    fontSize: hp(14),
+    lineHeight: hp(21),
+    color: 'red',
   },
 });
