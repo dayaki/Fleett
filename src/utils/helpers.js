98b974bf24c95f2eafbc20a9fba56ac8348e0c04
Dayo Akinkuowo <me@sprypixels.com>
2021-03-25 09:57:56 +0100

    Installed needed packages

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,8 @@
+import Geocoder from 'react-native-geocoding';
 import { store } from '../store';
 import { RESET_LOADER } from '../store/actions/types';
+
+Geocoder.init('AIzaSyC3txceMWvnO5IwXxkOkMZ_EkK1RyE0Ad4', { language: 'en' });
 
 export const checkAuth = async () => {
   store.dispatch({ type: RESET_LOADER });
